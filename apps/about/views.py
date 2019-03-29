0c5b38553899d9e59ccd002644eacde87b0edc18
Timo Halbesma <timohalbesma@gmail.com>
2019-03-29 15:45:19 +0000

    Drop some Bokeh example plot

--- apps/about/views.py
+++ apps/about/views.py
@@ -1,39 +1,43 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from catalogue.models import Reference
+from catalogue.models import Observation
+from catalogue.models import GlobularCluster
+
 
 def index(request):
-#     reference =  get_object_or_404(Reference, name__startswith="Harri")
-#     obj = Observation.objects.filter(name=reference)
-#     cs = GlobularCluster.objects.all()
-#     obj = [Observation.objects.filter(name=reference, name=c) for c in cs]
-#     names = np.array([o.name for o in cs]    )
-#     ra =    np.array([o.filter(pname="RA")[0].val for o in obj]      )
-#     dec =    np.array([o.filter(pname="Dec")[0].val for o in obj]      )
-#     l =    np.array([o.filter(pname="L")[0].val for o in obj]      )
-#     b =    np.array([o.filter(pname="B")[0].val for o in obj]      )
-#     ra =    np.array([o.filter(pname="RA")[0].val for o in obj]      )
-#     ra =    np.array([o.filter(pname="RA")[0].val for o in obj]      )
-#     urls = ["cluster/"+o.name for o in cs]
-#     l[l>180] = l[l>180]-360.
-#
-#     fig = plt.figure(2, figsize=(16,8))
-#     ax = plt.subplot(111)
-#
-#     points = ax.scatter(l, b, c="r", s=100, alpha=0.5, cmap=plt.cm.jet)
-#     ax.grid(color="white", linestyle="solid")
-#     ax.set_title("Globular clusters", size=20)
-#     ax.set_xlabel("l [deg]", size=20)
-#     ax.set_ylabel("b [deg]", size=20)
-#
-#     mpld3.plugins.connect(fig, ClickInfo(points, urls))
-#
-#     labels = ["<table><tr>{}<td></tr></td>".format(n) for n in names]
-#     tooltip = mpld3.plugins.PointHTMLTooltip(points, labels=labels, css=css)
-#     mpld3.plugins.connect(fig, tooltip)
-#
-#     result = mpld3.fig_to_html(fig)
-#
-    result = None
-    return render(request, "about/index.html", {"figure": result})
+    harris1996ed2010 = get_object_or_404(Reference, bib_code="1996AJ....112.1487H")
+    # clusters = GlobularCluster.objects.filter(references=harris1996ed2010)
+    clusters = GlobularCluster.objects.all()
+    names = np.array([o.name for o in clusters])
+    ra = np.array([c.observation_set.filter(parameter__name="RA") for c in clusters])
+    dec = np.array([c.observation_set.filter(parameter__name="Dec") for c in clusters])
+    l = np.array([c.observation_set.filter(parameter__name="L") for c in clusters])
+    b = np.array([c.observation_set.filter(parameter__name="B") for c in clusters])
+    # urls = ["cluster/"+o.name for o in cs]
+    # l[l>180] = l[l>180] - 360.
+
+    from bokeh.plotting import figure, show, output_file
+    from bokeh.sampledata.iris import flowers
+    from bokeh.embed import components
+
+    colormap = {'setosa': 'red', 'versicolor': 'green', 'virginica': 'blue'}
+    colors = [colormap[x] for x in flowers['species']]
+
+    p = figure(title = "Iris Morphology")
+    p.xaxis.axis_label = 'Petal Length'
+    p.yaxis.axis_label = 'Petal Width'
+    p.circle(flowers["petal_length"], flowers["petal_width"],
+             color=colors, fill_alpha=0.2, size=10)
+    fig_script, fig_div = components(p)
+
+    return render(request, "about/index.html", {
+        "fig_script": fig_script, "fig_div": fig_div})
+
 
 def info(request):
     return render(request, "about/info.html", {})
