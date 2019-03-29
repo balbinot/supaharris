2288d8286286a2e7d2bd4ef023cebd67803c9540
Timo Halbesma <timohalbesma@gmail.com>
2019-03-29 20:47:49 +0100

    Some production updates

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -1,7 +1,11 @@
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, os.path.abspath(os.path.join(root_path, 'src')))
+sys.path.insert(0, os.path.abspath('/srv/supaharris/venv3/lib/python3.5/site-packages/'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
