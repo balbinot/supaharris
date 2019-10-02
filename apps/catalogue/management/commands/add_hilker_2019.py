996bf414023157147ee35b72cc8b5cb855ffbac8
Timo Halbesma <timohalbesma@gmail.com>
2019-10-02 17:38:21 +0200

    For now only create Reference instances for Hilker+ 2019 #2

--- apps/catalogue/management/commands/add_hilker_2019.py
+++ apps/catalogue/management/commands/add_hilker_2019.py
@@ -4,7 +4,6 @@ import sys
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.core.management.base import CommandError
 
 from catalogue.models import (
     Reference,
@@ -31,65 +30,77 @@ class Command(PrepareSupaHarrisDatabaseMixin, BaseCommand):
         cmd = __file__.split("/")[-1].replace(".py", "")
         print("\n\nRunning the management command '{0}'\n".format(cmd))
 
-        # Add the ADS url (in this particular format). When the Reference
-        # instance is saved it will automatically retrieve all relevent info!
-        ads_url = "https://ui.adsabs.harvard.edu/abs/1996AJ....112.1487H"
-        reference, created = Reference.objects.get_or_create(ads_url=ads_url)
+        # Copy-paste from arXiv:1908.02778v1:
+        # Baumgardt (2017) first compared a large grid of 900 N-body models to
+        # the velocity dispersion and surface brightness profiles of 50 GGCs
+        # in order to determine their masses and mass-to-light ratios.
+        #
+        # Baumgardt 2017, https://arxiv.org/pdf/1609.08794.pdf
+        # http://simbad.u-strasbg.fr/simbad/sim-ref?querymethod=bib&simbo=on&submit=submit+bibcode&bibcode=2017MNRAS.464.2174B
+        ads_url = "https://ui.adsabs.harvard.edu/abs/2017MNRAS.464.2174B"
+        baumgardt2017, created = Reference.objects.get_or_create(ads_url=ads_url)
         if not created:
-            print("Found the Reference: {0}\n".format(reference))
+            print("Found the Reference: {0}\n".format(baumgardt2017))
         else:
-            print("Created the Reference: {0}\n".format(reference))
+            print("Created the Reference: {0}\n".format(baumgardt2017))
 
-
-
-        return
-
-        # Here we get one particular parameter as an example to help you
-        # See https://docs.djangoproject.com/en/2.2/topics/db/queries/
-        # for an explanation of how to retrieve/create items from/in the database.
-        R_Sun = Parameter.objects.filter(name="R_Sun").first()
-        if R_Sun:
-            print("\nThe parameter is available: R_Sun.name = {0}".format(R_Sun.name))
+        # Copy-paste from arXiv:1908.02778v1:
+        # Additionally, Sollima & Baumgardt (2017) presented the global mass
+        # functions of 35 GGCs based on deep HST photometry in combination
+        # with multimass dynamical models.
+        #
+        # Sollima & Baumgardt (2017), https://arxiv.org/pdf/1708.09529.pdf
+        ads_url = "https://ui.adsabs.harvard.edu/abs/2017MNRAS.471.3668S"
+        sollima_baumgardt_2017, created = Reference.objects.get_or_create(ads_url=ads_url)
+        if not created:
+            print("Found the Reference: {0}\n".format(sollima_baumgardt_2017))
         else:
-            print("\nThe parameter is not available. It may be needed to create it")
-            # See apps/catalogue/models.py for the definition and requirements of Parameter
-            R_Sun = Parameter.objects.create(
-                name="R_Sun",  # must be a string, max 64 characters
-                description="Distance to the Sun",  # must be a string, max. 256 characters
-                unit="kpc",  # must be a string, max 63 characters. Note that the unit must comply with astropy.unit.
-                scale=1.0  # must be a float. This is the scale by which parameters must be multiplied by.
-            )
+            print("Created the Reference: {0}\n".format(sollima_baumgardt_2017))
+
+        # Copy-paste from arXiv:1908.02778v1:
+        # One year later, Baumgardt & Hilker (2018) deter- mined masses, stellar
+        # mass functions, and structural parameters of 112 GGCs by fitting a
+        # large set of N-body simulations to their velocity dispersion and surface
+        # density profiles.
+        #
+        # Baumgardt & Hilker 2018, https://arxiv.org/pdf/1804.08359.pdf
+        ads_url = "https://ui.adsabs.harvard.edu/abs/2018MNRAS.478.1520B"
+        Baumgardt_Hilker_2018, created = Reference.objects.get_or_create(ads_url=ads_url)
+        if not created:
+            print("Found the Reference: {0}\n".format(Baumgardt_Hilker_2018))
+        else:
+            print("Created the Reference: {0}\n".format(Baumgardt_Hilker_2018))
+
+        # Copy-paste from arXiv:1908.02778v1:
+        # When Gaia DR2 became public, Baumgardt et al. (2019) presented mean
+        # proper motions and space velocities of 154 GGCs and the velocity
+        # dispersion profiles of 141 globular clusters based on a combination
+        # of Gaia DR2 proper motions with ground-based line-of-sight velocities.
+        # The combination of these velocity dispersion profiles with new
+        # measurements of the internal mass functions allowed to model the internal
+        # kinematics of 144 GGCs, more than 90 per cent of the currently known
+        # Milky Way globular cluster population
+        #
+        # Baumgardt, Hilker, Sollima & Bellini 2019, https://arxiv.org/pdf/1811.01507.pdf
+        ads_url = "https://ui.adsabs.harvard.edu/abs/2019MNRAS.482.5138B"
+        BHSB2019, created = Reference.objects.get_or_create(ads_url=ads_url)
+        if not created:
+            print("Found the Reference: {0}\n".format(BHSB2019))
+        else:
+            print("Created the Reference: {0}\n".format(BHSB2019))
+        # J/MNRAS/482/5138/table1
+        # J/MNRAS/482/5138/table4
+
+        # Finally, Sollima, Baumgardt & Hilker (2019) analysed the internal
+        # kinematics of 62 GGCs, finding significant rotation in 15 of them.
+        # https://arxiv.org/pdf/1902.05895.pdf
+        ads_url = "https://ui.adsabs.harvard.edu/abs/2019MNRAS.485.1460S"
+        SBH2019, created = Reference.objects.get_or_create(ads_url=ads_url)
+        if not created:
+            print("Found the Reference: {0}\n".format(SBH2019))
+        else:
+            print("Created the Reference: {0}\n".format(SBH2019))
 
         hilker_2019_orbits = parse_hilker_2019_orbits()
-        for entry in database:
-            gc_name = entry[0]
-            gc_R_Sun = entry[1]
-
-            print("gc_name: {0}".format(gc_name))
-            print("gc_R_Sun: {0}".format(gc_R_Sun))
-
-            # The Globular Cluster for which you would like to add new data could
-            # very well already be known in the database. In that case you should
-            # retrieve the AstroObject, and add an Observation
-            # to add may
-            gc, created = AstroObject.objects.get_or_create(name=gc_name)
-            if not created:
-                print("Found the AstroObject: {0}".format(gc))
-            else:
-                print("Created the AstroObject: {0}".format(gc))
-
-            # self.GC is set in PrepareSupaHarrisDatabaseMixin. If another
-            # classification, or multiple classifications apply, use e.g.
-            # YMC = AstroObjectClassification.objects.get(name="Young Massive Cluster")
-            # gc.classifications.add(YMC)
-            gc.classifications.add(self.GC)
-            gc.save()
-
-            observation = Observation.objects.create(
-                reference=reference,
-                astro_object=gc,
-                parameter=R_Sun,
-                value=gc_R_Sun,
-            )
-            print("Created the Observation: {0}".format(observation))
-
+        hilker_2019_combined = parse_hilker_2019_combined()
+        hilker_2019_radial_velocities = parse_hilker_2019_radial_velocities()
